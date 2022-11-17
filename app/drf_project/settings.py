bab411e07e1b4722d18566ca231bbcea19a36926
Chris G <cgillispie@gmail.com>
2022-11-17 14:49:47 -0600

    some app platform settings

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -142,3 +142,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# disable browsable API in prod
+if not DEBUG:
+    REST_FRAMEWORK = {
+        "DEFAULT_RENDERER_CLASSES": (
+            "rest_framework.renderers.JSONRenderer",
+        )
+    }
