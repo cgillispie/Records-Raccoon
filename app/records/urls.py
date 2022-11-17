b9ceb98c05a603b36bb76a4d4a39e396c9eb5039
Chris Gillispie <17147563+cgillispie@users.noreply.github.com>
2022-11-17 16:22:52 -0600

    Update urls.py

--- app/records/urls.py
+++ app/records/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import MovieList, MovieDetail, RecordList, RecordDetail
+from drf_project.views import homepage
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path("api/movies/<int:pk>/", MovieDetail.as_view()),
     path("api/records/", RecordList.as_view()),
     path("api/records/<int:pk>/", RecordDetail.as_view()),
+    path('/', homepage, name='homepage'),
 ]
